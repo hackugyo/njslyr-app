80f3afab9ae82279f58b9fa8fab38f3a9221784e
hackugyo <game6000yen@gmail.com>
2011-06-30 04:57:49 +0900

    rails new njslyr-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NjslyrApp::Application.config.secret_token = '7a6f4d8496055516d137193505f889bd435d6be87832894fceb100fdb5d302112b7dbfcedc0a7faf8d49394f73b41cceff7c69772c771bb84d670bdb2494d02d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NjslyrApp::Application.config.session_store :cookie_store, :key => '_njslyr-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NjslyrApp::Application.config.session_store :active_record_store
